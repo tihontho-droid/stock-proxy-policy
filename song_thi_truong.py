7cf48f38941c4e786c2243329a72452f660e0bc8
tihontho-droid <tihontho@gmail.com>
2026-06-10 15:53:51 +0700

    Update song_thi_truong.py

--- song_thi_truong.py
+++ song_thi_truong.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import requests
 
+st.subheader("VNINDEX - Tín hiệu tạo đáy")
+
+url = "https://stocktraders.vn/service/data/getTotalTrade"
+
+payload = {
+    "TotalTradeRequest": {
+        "account": "uyen.png"
+    }
+}
+
+response = requests.post(url, json=payload)
+
+st.write(response.status_code)
