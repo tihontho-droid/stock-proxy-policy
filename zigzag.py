de30201e8aa9dc9202600a2f999f62e4f7e08fdc
tihontho-droid <tihontho@gmail.com>
2026-06-23 10:12:29 +0700

    Update zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -5,7 +5,7 @@ from streamlit_lightweight_charts import renderLightweightCharts
 
 st.set_page_config(layout="wide")
 
-st.title("Giao dịch theo sóng thị trường")
+st.title("Giao dịch theo sóng thị trường") 
 
 ticker_list_202 = [
     "AAA", "ABB", "ACB", "AGG", "AGR", "ANV", "APG", "APS", "ASM",
